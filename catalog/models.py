efd67e316e95796ce7f5e71d761d765a1db0d7f3
VladimirBek <vladimirbek1996@gmail.com>
2023-08-03 23:10:18 +0300

    alter_category

--- catalog/models.py
+++ catalog/models.py
@@ -7,6 +7,13 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='категория')
     description = models.TextField(verbose_name='описание', **NULLABLE)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
