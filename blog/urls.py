15757e021d755fb3cb663b0c3460061171039cbc
VladimirBek <vladimirbek1996@gmail.com>
2023-09-03 21:02:39 +0300

    update urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from .views import IndexList, BlogDetail, BlogCreate, BlogUpdate, BlogDelete
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('blog/', IndexList.as_view(), name='index'),
-    path('blog/<slug:slug>/', BlogDetail.as_view(), name='articles'),
+    path('', IndexList.as_view(), name='index'),
+    path('<slug:slug>/', BlogDetail.as_view(), name='articles'),
     path('create_article/', BlogCreate.as_view(), name='create_article'),
     path('update_article/<slug:slug>/', BlogUpdate.as_view(), name='update_article'),
     path('delete_article/<slug:slug>/', BlogDelete.as_view(), name='delete_article'),
